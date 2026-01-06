c338035fdfccefb0674fa1c47754563742708df9
jenni <jenni@DaddysComp>
2026-01-05 16:29:09 -0800

    Local machine commit and push.

---
+++ week1lab/src/week1lab/Week1Lab.java
@@ -0,0 +1,12 @@
+package week1lab;
+
+public class Week1Lab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+System.out.println();
+System.out.println("Congratulations on your first Java program");
+	}
+
+}

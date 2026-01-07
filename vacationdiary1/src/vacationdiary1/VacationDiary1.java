ae5579e96d55afec7a5026d039c1356f6d32c641
jenni <jenni@DaddysComp>
2026-01-06 16:26:53 -0800

    vacations diary 1

---
+++ vacationdiary1/src/vacationdiary1/VacationDiary1.java
@@ -0,0 +1,55 @@
+//Nathan Jennings CIS406 Vacation Diary 1//
+package vacationdiary1;
+
+import java.util.Scanner;
+
+public class VacationDiary1 {
+
+	public static void main(String[] args) {
+
+//Open the Scanner object to receive user input.
+Scanner input = new Scanner(System.in);
+		
+		
+//name of program
+System.out.println("Vacation Diary");
+
+//Enter date vacation started.
+System.out.print("Enter date of vacation started (mm/dd/yyyy): ");
+String date = input.next();
+
+//Enter the city visited.
+System.out.print("Enter city visited: ");
+String city = input.next();
+
+//Enter the country visited.
+System.out.print("Enter country visited: ");
+String country = input.next();
+
+//Enter the number of days.
+System.out.print("How many days were you there? ");
+String days = input.next();
+
+//Enter the mode of travel (car, airplane, ship, train, bus) to reach 
+//the destination.
+System.out.print("How did you get there? (car, airplane, ship, train, bus): ");
+String travelMode = input.next();
+
+
+System.out.println("");
+System.out.println("Details of your trip: ");
+System.out.println("Date: " + date);
+System.out.println("City: " + city);
+System.out.println("Country: " + country);
+System.out.println("Number of days: " + days);
+System.out.println("Traveled by: " + travelMode);
+System.out.println("");
+System.out.println("WooHoo, Hope you had an amazing time!!!");
+
+
+
+input.close();
+
+	}
+
+}
